2c4c128d86cfcae0a78c0ffd37663fa111c6fdb9
Ritika <riyachhikara26@gmail.com>
2025-11-01 19:10:41 +0530

    Change

--- Program8.java
+++ Program8.java
@@ -1,11 +1,11 @@
-class Program8
-{
-	public static void main(String[] args)
-	{
-		System.out.println("Main Starts");
-		for(int i=1,j=10;i<=10;i++,j--)
-		{
-			System.out.println("i: "+i+" j: "+j);
-		}
-	}
+
+class Program8 {
+
+    public static void main(String[] args) {
+        System.out.println("Main Starts");
+        for (int i = 1, j = 10; i <= 10; i++, j--) {
+            System.out.println("i: " + i + " j: " + j);
+        }
+    }
+
 }
